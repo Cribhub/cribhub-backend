0c41a6ce05ea754b25cf2af349a9bae6dc815f78
Marius Hatland <marius.hatland@student.uib.no>
2024-04-10 12:21:17 +0200

    added logging to task controller

--- src/main/java/com/cribhub/backend/controllers/TaskController.java
+++ src/main/java/com/cribhub/backend/controllers/TaskController.java
@@ -2,9 +2,8 @@ package com.cribhub.backend.controllers;
 
 import com.cribhub.backend.DTO.TaskDTO;
 import com.cribhub.backend.domain.Task;
-import com.cribhub.backend.services.CribService;
-import com.cribhub.backend.services.CustomerService;
 import com.cribhub.backend.services.TaskService;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,22 +12,21 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/tasks")
 @CrossOrigin
+@Log4j2
 public class TaskController {
 
     private final TaskService taskService;
-    private final CribService cribService;
-    private final CustomerService customerService;
 
     @Autowired
-    public TaskController(TaskService taskService, CribService cribService, CustomerService customerService) {
+    public TaskController(TaskService taskService) {
         this.taskService = taskService;
-        this.cribService = cribService;
-        this.customerService = customerService;
     }
 
     @PostMapping("/{customerId}/{cribId}")
     public ResponseEntity<TaskDTO> createTask(@PathVariable Long cribId, @PathVariable Long customerId, @RequestBody Task task) {
         Task savedTaskList = taskService.createTask(cribId, customerId, task );
+
+       log.info("Task created: {} {} {}", savedTaskList.getTaskId(), savedTaskList.getTaskName(), savedTaskList.getDescription());
         return ResponseEntity.status(HttpStatus.CREATED).body(TaskDTO.TaskUpdateDTO(savedTaskList));
 
     }
@@ -37,6 +35,7 @@ public class TaskController {
     public ResponseEntity<TaskDTO> getTaskById(@PathVariable Long taskId) {
         Task task = taskService.getTaskById(taskId);
         if (task == null){
+            log.error("Task with id {} not found", taskId);
             return ResponseEntity.notFound().build();
         }
         return ResponseEntity.ok(TaskDTO.TaskUpdateDTO(task));
@@ -45,6 +44,8 @@ public class TaskController {
     @DeleteMapping("/{taskId}")
     public ResponseEntity<Void> deleteTask(@PathVariable Long taskId) {
         taskService.deleteTask(taskId);
+
+        log.warn("Task with id {} deleted", taskId);
         return ResponseEntity.noContent().build();
     }
 
