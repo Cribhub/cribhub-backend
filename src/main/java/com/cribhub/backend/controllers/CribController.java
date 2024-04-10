e0d8fd19e7e151a1cd60673e483237466cd5d0c3
Marius Hatland <marius.hatland@student.uib.no>
2024-04-10 12:00:41 +0200

    added logging in crib controller

--- src/main/java/com/cribhub/backend/controllers/CribController.java
+++ src/main/java/com/cribhub/backend/controllers/CribController.java
@@ -3,13 +3,18 @@ package com.cribhub.backend.controllers;
 import com.cribhub.backend.DTO.CribDTO;
 import com.cribhub.backend.domain.Crib;
 import com.cribhub.backend.services.CribService;
+import lombok.extern.log4j.Log4j2;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.logging.Logger;
+
 @RestController
 @RequestMapping("/cribs")
 @CrossOrigin
+@Log4j2
 public class CribController {
     private final CribService cribService;
 
@@ -20,6 +25,7 @@ public class CribController {
     @PostMapping
     public ResponseEntity<CribDTO> createCrib(@RequestBody Crib crib) {
         crib = cribService.saveCrib(crib);
+        log.info("Crib with id {} created", crib.getCribId());
         return ResponseEntity.status(HttpStatus.CREATED).body(CribDTO.ConvertToCribDTO(crib));
     }
 
@@ -27,14 +33,18 @@ public class CribController {
     public ResponseEntity<CribDTO> getCribById(@PathVariable Long cribId) {
         Crib crib = cribService.getCribById(cribId);
         if (crib == null) {
+            log.error("Crib with id {} not found", cribId);
             return ResponseEntity.notFound().build();
         }
+        log.info("Crib with id {} retrieved", cribId);
         return ResponseEntity.ok(CribDTO.ConvertToCribDTO(crib));
     }
 
     @DeleteMapping("/{cribId}")
     public ResponseEntity<Void> deleteCrib(@PathVariable Long cribId) {
         cribService.deleteCrib(cribId);
+
+        log.warn("Crib with id {} deleted", cribId);
         return ResponseEntity.noContent().build();
     }
 
